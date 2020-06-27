4fdc58ec3b5c6255d70b4837a8fc8c13602ae55c
Batandwa Mgutsi <B.mgutsi@gmail.com>
2020-06-27 19:38:48 +0200

    Implemnted moved source from experiement

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Copyright 2020 The LytOwl Team.
+ * Use of this source code is governed by an MIT license that can be
+ * found in the LICENSE file.
+ */
+
+export { ViewModelConsumer, BaseViewModel } from "./lib/react_mvvm";
